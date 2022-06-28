b890938b5ce10f70deff5947b2d9461bab62af3e
a.turekhanov <a.turekhanov@mycar.kz>
2022-06-28 09:33:50 +0600

    Initial Commit

---
+++ HW2.1.1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW2.1.1
+//
+//  Created by a.turekhanov on 28.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
